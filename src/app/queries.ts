b5133b67bfd77a31852cd081c1d66ecaff0aa27d
Yuxuan Zhao <yxzhao@Yuxuans-MacBook-Pro.local>
2025-01-14 11:20:19 +0100

    Update Queries

--- src/app/queries.ts
+++ src/app/queries.ts
@@ -1,29 +1,28 @@
-export const CPU_USAGE_QUERY = `timeseries cpuAvg = avg(dt.host.cpu.usage), by:{dt.entity.host, host.name}
+export const CPU_USAGE_QUERY = `timeseries cpuAvg = avg(host.cpu.usage), by:{dt.entity.host, host.name}
+| dedup dt.entity.host
 | fieldsRename hostId = dt.entity.host, hostName = host.name
 | lookup [
   timeseries {
-    idle=avg(dt.host.cpu.idle),
-    ioWait=avg(dt.host.cpu.iowait),
-    user=avg(dt.host.cpu.user),
-    system=avg(dt.host.cpu.system),
-    steal=avg(dt.host.cpu.steal)
+    idle=avg(host.cpu.idle),
+    ioWait=avg(host.cpu.iowait),
+    user=avg(host.cpu.user),
+    system=avg(host.cpu.system),
+    steal=avg(host.cpu.steal)
   },
   by:{dt.entity.host}
   | fieldsAdd idle = arrayAvg(idle), ioWait = arrayAvg(ioWait), user = arrayAvg(user), system = arrayAvg(system), steal = arrayAvg(steal)
   | fieldsAdd other = 100 - idle - ioWait - user - system - steal
 ], sourceField:hostId, lookupField:dt.entity.host, fields:{idle, ioWait, user, system, steal, other}`;
 
-export const RAM_USAGE_QUERY = `timeseries ramAvg=avg(cpu.ram),by:{dt.entity.host}`;
-
 export const getHostCpuUsageQuery = (hostId: string) => `timeseries {
-    idle=avg(dt.host.cpu.idle),
-    ioWait=avg(dt.host.cpu.iowait),
-    user=avg(dt.host.cpu.user),
-    system=avg(dt.host.cpu.system),
-    steal=avg(dt.host.cpu.steal)
+    idle=avg(host.cpu.idle),
+    ioWait=avg(host.cpu.iowait),
+    user=avg(host.cpu.user),
+    system=avg(host.cpu.system),
+    steal=avg(host.cpu.steal)
   },
   filter:{dt.entity.host == "${hostId}"}
   | fieldsAdd other = 100 - idle[] - ioWait[] - user[] - system[] - steal[]`;
 
 export const getHostAvgCpuQuery = (hostId: string) =>
-  `timeseries cpuAvg = avg(dt.host.cpu.usage), filter:{dt.entity.host == "${hostId}"}`;
+  `timeseries cpuAvg = avg(host.cpu.usage), filter:{dt.entity.host == "${hostId}"}`;
